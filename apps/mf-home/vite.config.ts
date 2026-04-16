7724de1fc88fb466018975bfee3394301f21481c
FABRIZZIO RENZO PINEDO ESPINOZA <fpinedo@sanfelipe.com>
2026-04-15 21:12:15 -0500

    feature:actualizacion de vite

--- apps/mf-home/vite.config.ts
+++ apps/mf-home/vite.config.ts
@@ -35,8 +35,11 @@ export default defineConfig(({ mode }) => {
   ],
   resolve: {
     alias: {
-      react:       path.resolve(__dirname, "../../node_modules/react"),
-      "react-dom": path.resolve(__dirname, "../../node_modules/react-dom"),
+      "react":                path.resolve(__dirname, "../../node_modules/react/index.js"),
+      "react/jsx-runtime":    path.resolve(__dirname, "../../node_modules/react/jsx-runtime.js"),
+      "react/jsx-dev-runtime":path.resolve(__dirname, "../../node_modules/react/jsx-dev-runtime.js"),
+      "react-dom":            path.resolve(__dirname, "../../node_modules/react-dom/index.js"),
+      "react-dom/client":     path.resolve(__dirname, "../../node_modules/react-dom/client.js"),
     }
   },
   server:  { port: 10502 },
